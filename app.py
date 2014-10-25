b7e6a9b0cae43e32015ecf338812f01fb0d8b33c
Rodney Hawkins <rodneyhawkins@gmail.com>
2014-10-25 13:08:41 +0200

    Convert indent to spaces chop

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	client = etcd.Client(host='172.17.42.1',port=4001)
-	node_id = os.environ.get('node_id')
-	leader = None
-	try:
-		result = client.read('/carrier_test/leader')
-		if result.value == node_id:
-			leader = "ME"
-		else:
-			leader = result.value
-	except:
-		pass
-	response = make_response(render_template('whoami.html', leader=leader))
+    client = etcd.Client(host='172.17.42.1',port=4001)
+    node_id = os.environ.get('node_id')
+    leader = None
+    try:
+        result = client.read('/carrier_test/leader')
+        if result.value == node_id:
+            leader = "ME"
+        else:
+            leader = result.value
+    except:
+        pass
+    response = make_response(render_template('whoami.html', leader=leader))
     response.headers['proudly-served-by'] = str(node_id)
     return response
 
