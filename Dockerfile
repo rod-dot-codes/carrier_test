b4821d6b25f23605048fadb54b1ec3218e6203b6
Rodney Hawkins <rodneyhawkins@gmail.com>
2014-10-25 09:30:07 +0200

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu
+RUN echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get install -y python python-pip supervisor uwsgi
+EXPOSE 5000
+ADD . /opt/apps/carrier_test
+RUN pip install -r /opt/apps/carrier_test/requirements.txt
+RUN chown -R www-data:www-data /opt/apps/carrier_test
+CMD ["/bin/sh", "-e", "/opt/apps/carrier_test/run"]
---
+++ leader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import etcd
+import time
+import random
+import os
+
+def whose_the_leader(node_id):
+	""" Check who the leader of the carrier_test application and if no-one is leader, make me the leader.
+	"""
+	while True:
+		client = etcd.Client(host='172.17.42.1',port=4001)
+		#Tell my peers that I exist
+		client.write('/carrier_test/%s', 1,ttl=61)
+		#Is there a leader available
+		result = client.read('/carrier_test/leader')
+		if result.value == None:
+			#Try to become leader
+			result.value = node_id
+			result.ttl = 10 #TTL of 10 seconds till it expires
+			try:
+				print "Trying to become the leader"
+				updated = Client.update(result)
+				print "I am the leader"
+			except:
+				print "Damn, someone became leader before me"
+		sleep_duration = int(random.randrange(0,60))
+		print "Sleeping for %s seconds" % (sleep_duration)
+
+if __name__ == "__main__":
+	node_id = print os.environ.get('node_id')
+	whose_the_leader(node_id)
